4fdeeceeb4a4b9802f399947064a4d121cd6dbd1
aminechaib <ccamine4@gmail.com>
2024-02-16 18:05:33 +0100

    update

--- source-code/admin/views/admin-profile/profile.php
+++ source-code/admin/views/admin-profile/profile.php
@@ -1,25 +1,24 @@
 <?php
-require_once('scripts/Profile.php');
-$profile= new Profile($conn);
+require_once('scripts/profile.php');
+$profile = new Profile($conn);
 $profileData = $profile->getProfile();
 
 ?>
 <div class="row">
-    <div class="col-sm-3">
+  <div class="col-sm-3">
 
     <div class="profile-picture">
-            <img src="public/images/admin-profile/<?= $profileData['profileImage']; ?>" width="200px">
-     </div>
-    
+      <img src="public/images/admin-profile/<?= $profileData['profileImage']; ?>" width="200px">
     </div>
-    <div class="col-sm-9">
-        <br>
-      <h3><?php echo $profileData['firstName'] . " ". $profileData['firstName'];  ?> </h3>
+
+  </div>
+  <div class="col-sm-9">
+    <br>
+    <h3><?php echo $profileData['firstName'] . " " . $profileData['firstName'];  ?> </h3>
     <p><?= $profileData['gender']; ?></p>
     <p><?= $profileData['emailAddress']; ?></p>
     <p><?= $profileData['mobileNumber']; ?></p>
 
-    </div>
-    
-</div>
+  </div>
 
+</div>
